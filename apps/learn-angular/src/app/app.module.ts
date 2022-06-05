400d43675c65a952f0b02026857eac7d92136def
xocomil <xocomil@gmail.com>
2022-06-05 13:20:26 -0600

    added angular material

--- apps/learn-angular/src/app/app.module.ts
+++ apps/learn-angular/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
