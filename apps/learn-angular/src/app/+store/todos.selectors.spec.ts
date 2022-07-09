00d8917c062bf2b19127884a0a4238df01b81d13
xocomil <xocomil@gmail.com>
2022-07-09 15:54:02 -0600

    use test factory

--- apps/learn-angular/src/app/+store/todos.selectors.spec.ts
+++ apps/learn-angular/src/app/+store/todos.selectors.spec.ts
@@ -1,14 +1,11 @@
-import { faker } from '@faker-js/faker/locale/en';
+import { createFakeTodoListItem } from '../models/testing/todo-list-item';
 import { TodoListItem } from '../models/todo-list-item';
 import { selectTodoList } from './todos.selectors';
 
 describe('todos.selectors', () => {
   describe('selectTodoList', () => {
     it('should return the list of todos', () => {
-      const todos: TodoListItem[] = Array.from({ length: 5 }, () => ({
-        done: faker.datatype.boolean(),
-        description: faker.random.words(),
-      }));
+      const todos: TodoListItem[] = Array.from({ length: 5 }, createFakeTodoListItem);
 
       expect(selectTodoList.projector({ todos })).toEqual(todos);
     });
