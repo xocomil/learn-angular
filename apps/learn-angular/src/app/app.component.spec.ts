99b616de2b17c0a13a8e4f822f33ac6643c68a1e
xocomil <xocomil@gmail.com>
2022-08-20 17:02:45 -0600

    updated all of our tests to pass

--- apps/learn-angular/src/app/app.component.spec.ts
+++ apps/learn-angular/src/app/app.component.spec.ts
@@ -1,29 +1,11 @@
-import { FormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { faker } from '@faker-js/faker/locale/en';
 import { createComponentFactory } from '@ngneat/spectator/jest';
 import { Store } from '@ngrx/store';
-import { provideMockStore } from '@ngrx/store/testing';
-import { addTodo, clearAll, deleteTodo, updateChecked } from './+store/todos.actions';
-import { State } from './+store/todos.reducers';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  const description = 'Test todo item description';
-
-  const initialState: { todoState: State } = {
-    todoState: {
-      todos: [{ description, done: false }],
-    },
-  };
-
   const createComponent = createComponentFactory({
     component: AppComponent,
-    imports: [FormsModule, MatCardModule, MatCheckboxModule, MatFormFieldModule, MatInputModule],
-    providers: [provideMockStore({ initialState })],
+    mocks: [Store],
   });
 
   it('should create the app', () => {
@@ -36,79 +18,5 @@ describe('AppComponent', () => {
     const spectator = createComponent();
 
     expect(spectator.fixture).toMatchSnapshot();
-  });
-
-  describe('addNewTodo()', () => {
-    it('should dispatch an addNewTodo action', () => {
-      const newTodoDescription = faker.random.words();
-
-      const spectator = createComponent({ props: { newTodoDescription } });
-      const store = spectator.inject(Store);
-      const dispatchSpy = jest.spyOn(store, 'dispatch');
-
-      const expected = addTodo({ todoListItem: { description: newTodoDescription, done: false } });
-
-      spectator.component.addNewTodo();
-
-      expect(dispatchSpy).toHaveBeenCalledWith(expected);
-    });
-
-    it('should clear the newTodoDescription property', () => {
-      const newTodoDescription = faker.random.words();
-
-      const spectator = createComponent({ props: { newTodoDescription } });
-
-      expect(spectator.component.newTodoDescription).toBe(newTodoDescription);
-
-      spectator.component.addNewTodo();
-
-      expect(spectator.component.newTodoDescription).toBe('');
-    });
-  });
-
-  describe('clearAll()', () => {
-    it('should dispatch a clearAll action', () => {
-      const spectator = createComponent();
-      const store = spectator.inject(Store);
-      const dispatchSpy = jest.spyOn(store, 'dispatch');
-
-      const expected = clearAll();
-
-      spectator.component.clearAll();
-
-      expect(dispatchSpy).toHaveBeenCalledWith(expected);
-    });
-  });
-
-  describe('checkedStateChanged()', () => {
-    [true, false].forEach((testCase) => {
-      it(`should dispatch a updateChecked action: [${testCase}]`, () => {
-        const spectator = createComponent();
-        const store = spectator.inject(Store);
-        const dispatchSpy = jest.spyOn(store, 'dispatch');
-        const arrayIndex = faker.datatype.number({ min: 0, max: 100 });
-
-        const expected = updateChecked({ arrayIndex, checked: testCase });
-
-        spectator.component.checkedStateChanged({ checked: testCase } as MatCheckboxChange, arrayIndex);
-
-        expect(dispatchSpy).toHaveBeenCalledWith(expected);
-      });
-    });
-  });
-
-  describe('deleteItem()', () => {
-    it('should dispatch a deleteTodo action', () => {
-      const spectator = createComponent();
-      const store = spectator.inject(Store);
-      const dispatchSpy = jest.spyOn(store, 'dispatch');
-      const arrayIndex = faker.datatype.number({ min: 0, max: 100 });
-
-      const expected = deleteTodo({ arrayIndex });
-
-      spectator.component.deleteItem(arrayIndex);
-
-      expect(dispatchSpy).toHaveBeenCalledWith(expected);
-    });
   });
 });
