bafce4ec67972d526f777c86927482f047f5aa21
xocomil <xocomil@gmail.com>
2022-12-18 16:58:31 -0700

    show how to type an observable using filter

--- libs/typescript-tricks/src/lib/typescript-tricks/typescript-tricks.component.ts
+++ libs/typescript-tricks/src/lib/typescript-tricks/typescript-tricks.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { AsConstComponent } from '../components/as-const/as-const.component';
+import { ObservableFilterComponent } from '../components/observable-filter/observable-filter.component';
 
 @Component({
   selector: 'ts-tricks-typescript-tricks',
   standalone: true,
-  imports: [CommonModule, AsConstComponent],
-  template: ` <ts-tricks-as-const></ts-tricks-as-const> `,
+  imports: [CommonModule, AsConstComponent, ObservableFilterComponent],
+  template: `
+    <ts-tricks-as-const></ts-tricks-as-const>
+    <ts-tricks-observable-filter></ts-tricks-observable-filter>
+  `,
   styleUrls: ['./typescript-tricks.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush,
